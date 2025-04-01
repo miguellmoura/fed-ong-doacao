5bddb2b54de6fcbce21d4ea10b2561266819d388
miguellmoura <miguelmouracvm23@gmail.com>
2025-03-31 21:05:48 -0300

    feat: Add sprints pics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StoriesComponent } from './stories/stories.component';
 import { ActionsComponent } from './actions/actions.component';
+import { ParticipateComponent } from './participate/participate.component';
+import { DonateComponent } from './donate/donate.component';
+import { MissionComponent } from './mission/mission.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, StoriesComponent, ActionsComponent],
+  imports: [RouterOutlet, HomeComponent, StoriesComponent, ActionsComponent, ParticipateComponent, DonateComponent, MissionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
